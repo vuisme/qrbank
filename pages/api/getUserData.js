4127072732bb21860f90de4f5eb09ecf3429bc1b
vuisme <me@vutn.net>
2025-01-24 17:01:03 +0700

    change to postgress

--- pages/api/getUserData.js
+++ pages/api/getUserData.js
@@ -4,11 +4,15 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     const { user } = req.query;
 
+    if (!user) { // Kiểm tra xem user có được truyền vào không
+      return res.status(400).json({ error: 'User ID is required.' });
+    }
+
     try {
       // Lấy thông tin người dùng từ database dựa vào userid
       const results = await query({
-        query: 'SELECT bank_code, bank_account FROM members WHERE userid = $1',
-        values: [user],
+        query: 'SELECT bank_code, bank_account FROM members WHERE userid = $1', // Thêm điều kiện WHERE
+        values: [user], // Truyền userid vào values
       });
 
       if (results.length > 0) {
