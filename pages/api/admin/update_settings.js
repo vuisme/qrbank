668a3930f484de043fa1d813702571cb0a70ee20
vuisme <me@vutn.net>
2025-01-24 16:48:22 +0700

    change top postgress

--- pages/api/admin/update_settings.js
+++ pages/api/admin/update_settings.js
@@ -5,26 +5,19 @@ export default async function handler(req, res) {
     const { apiKey, apiServer } = req.body;
 
     try {
-      // Kiểm tra xem bảng settings có dữ liệu chưa
-      const settingsCount = await query({
-        query: 'SELECT count(*) as count FROM settings',
-      });
+        console.log("Upserting settings...");
+        const settingsUpdated = await query({
+          query: `INSERT INTO settings (id, api_key, api_server) VALUES (1, $1, $2)
+          ON CONFLICT (id) 
+          DO 
+            UPDATE SET api_key = $1, api_server = $2`,
+          values: [apiKey, apiServer],
+        });
 
-      if (settingsCount[0].count === 0) {
-        // Nếu bảng trống, thực hiện INSERT
-        console.log("Inserting new settings...");
-        await query({
-          query: 'INSERT INTO settings (api_key, api_server) VALUES ($1, $2)',
-          values: [apiKey, apiServer],
-        });
-      } else {
-        // Nếu bảng có dữ liệu, thực hiện UPDATE
-        console.log("Updating existing settings...");
-        await query({
-          query: 'UPDATE settings SET api_key = $1, api_server = $2 WHERE id = 1', // Giả định id của settings là 1
-          values: [apiKey, apiServer],
-        });
-      }
+        // Kiểm tra xem có dòng nào bị ảnh hưởng không
+        if (settingsUpdated === 0) {
+          return res.status(404).json({ error: 'Settings not found.' });
+        }
 
       res.status(200).json({ message: 'Settings updated successfully.' });
     } catch (error) {
