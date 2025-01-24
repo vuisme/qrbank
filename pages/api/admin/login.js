fbe66224720c38d3bbfaeab6b755170c9d38b06b
vuisme <me@vutn.net>
2025-01-24 18:11:05 +0700

    change to postgress

--- pages/api/admin/login.js
+++ pages/api/admin/login.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     try {
       // Lấy thông tin admin từ database
       const results = await query({
-        query: 'SELECT * FROM superadmin WHERE user = $1',
+        query: 'SELECT * FROM superadmin WHERE username = $1',
         values: [username],
       });
 
