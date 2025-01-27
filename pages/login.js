eea6c718e39853f8a1df4e22fb1135ebffea5860
vuisme <me@vutn.net>
2025-01-27 19:11:17 +0700

    add cache redis user bank

--- pages/login.js
+++ pages/login.js
@@ -9,6 +9,7 @@ import {
   Alert,
 } from '@mui/material';
 import withReCAPTCHA from '../components/withReCAPTCHA'; // Import HOC
+import Meta from '../components/Meta';
 
 function Login({ recaptchaToken }) {
   // Nhận recaptchaToken từ props
@@ -43,48 +44,59 @@ function Login({ recaptchaToken }) {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        {error && <Alert severity="error">{error}</Alert>}
-        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="userid"
-            label="User ID"
-            name="userid"
-            autoFocus
-            value={userid}
-            onChange={(e) => setUserid(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+    <>
+      <Meta
+        title="Gửi mã QR Thần Tốc - MãQR.TOP"
+        description= "Công cụ Tạo và Gửi mã QR với số tiền tùy chọn cực kỳ nhanh chóng"
+        keywords="quét mã qr, thanh toán, vietqr, ngân hàng, chuyển tiền, qr code, maqr"
+        ogTitle="Gửi mã QR Thần Tốc - MãQR.TOP"
+        ogDescription="Công cụ Tạo và Gửi mã QR với số tiền tùy chọn cực kỳ nhanh chóng"
+        ogImage="/qr-code-animation.gif"
+        ogUrl="https://maqr.top"
+      />
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
             Login
-          </Button>
+          </Typography>
+          {error && <Alert severity="error">{error}</Alert>}
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="userid"
+              label="User ID"
+              name="userid"
+              autoFocus
+              value={userid}
+              onChange={(e) => setUserid(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Login
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </>  
   );
 }
 
